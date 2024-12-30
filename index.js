31ed7a2d89ba3bfb4b98a388054665448d1a02df
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2024-12-29 16:16:58 -0800

    completed functions for buttons

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-homeScore = document.getElementById("home-score")
-guestScore = document.getElementById("guest-score")
+homeScore = 0
+guestScore = 0
+homeScoreDisplay = document.getElementById("home-score")
+guestScoreDisplay = document.getElementById("guest-score")
 
 function addOneHome() {
-    
+    homeScore += 1
+    homeScoreDisplay.textContent = homeScore    
 }
 
 function addTwoHome() {
-    
+    homeScore += 2
+    homeScoreDisplay.textContent = homeScore    
 }
 
 function addThreeHome() {
-    
+    homeScore += 3
+    homeScoreDisplay.textContent = homeScore
 }
 
 function addOneGuest() {
-    
+    guestScore += 1
+    guestScoreDisplay.textContent = guestScore        
 }
 
 function addTwoGuest() {
-    
+    guestScore += 2
+    guestScoreDisplay.textContent = guestScore            
 }
 
 function addThreeGuest() {
+    guestScore += 3
+    guestScoreDisplay.textContent = guestScore        
     
 }

4896ecc2b8fa8fa1f637aa46ad84134e3b43df10
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2024-12-29 16:07:28 -0800

    fixed function names for Guest buttons, added function skeletons

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+homeScore = document.getElementById("home-score")
+guestScore = document.getElementById("guest-score")
 
+function addOneHome() {
+    
+}
+
+function addTwoHome() {
+    
+}
+
+function addThreeHome() {
+    
+}
+
+function addOneGuest() {
+    
+}
+
+function addTwoGuest() {
+    
+}
+
+function addThreeGuest() {
+    
+}
